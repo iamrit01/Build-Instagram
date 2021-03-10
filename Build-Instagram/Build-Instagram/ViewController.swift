4b1474f7651bf64fe932a688220139fa104d7204
Mízia Lima <mizia.alima@gmail.com>
2021-03-09 22:38:14 -0300

    Cria base do projeto.

---
+++ Build-Instagram/Build-Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Build-Instagram
+//
+//  Created by Mizia Lima on 3/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Build-Instagram/Build-InstagramTests/Build_InstagramTests.swift
@@ -0,0 +1,33 @@
+//
+//  Build_InstagramTests.swift
+//  Build-InstagramTests
+//
+//  Created by Mizia Lima on 3/9/21.
+//
+
+import XCTest
+@testable import Build_Instagram
+
+class Build_InstagramTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
